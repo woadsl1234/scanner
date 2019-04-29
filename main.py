d8be18bbf7400b0dec2c15d4c8405eb0d169876c
apple <491565693@qq.com>
2019-04-29 23:03:01 +0800

    start

---
+++ main.py
@@ -0,0 +1,21 @@
+import argparse
+import logging
+import sys
+
+def run(args):
+    pass
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="scanner")
+    parser.add_argument("-d","--domain",metavar="",
+        help="domain name")
+    parser.add_argument("-o","--out",metavar="",default="domains.log",
+        help="result out file")
+    args = parser.parse_args()
+
+    try:
+        run(args)
+    except KeyboardInterrupt:
+        logging.info("Ctrl C - Stopping Client")
+        sys.exit(1)
